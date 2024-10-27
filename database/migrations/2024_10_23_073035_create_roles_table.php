fe1ddcdff50adb2395a552d92b62bf27b76de23e
Matthew <m.matthew.muliawan@gmail.com>
2024-10-27 15:46:08 +0700

    add Role Seeder

--- database/migrations/2024_10_23_073035_create_roles_table.php
+++ database/migrations/2024_10_23_073035_create_roles_table.php
@@ -13,12 +13,16 @@ return new class extends Migration
      */
     public function up()
     {
+        
+        
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string("name");
-            $table->string("description");
+            $table->string("subtitle");
+            $table->text("description");
             $table->string("passive");
+            $table->string("banner");
             $table->string("img");
 
         });
--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Role;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class RoleSeeder extends Seeder
 {
@@ -14,6 +16,18 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $json = file_get_contents(database_path('data/role.json'));
+        $data = json_decode($json, true);
+
+        foreach($data as $d){
+            Role::create([
+                'name' => $d['name'],
+                'subtitle' => $d['subtitle'],
+                'description' => $d['description'],
+                'passive' => $d['passive'],
+                'banner' => $d['banner'],
+                'img' => $d['img']
+            ]);
+        }
     }
 }
