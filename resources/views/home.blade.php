e397eae2f4d6f99b41ebb6475fe689002e1fe890
Matthew <m.matthew.muliawan@gmail.com>
2024-10-27 14:18:43 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bootstrap Example</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="alert alert-success" role="alert">
+            Bootstrap is connected successfully!
+        </div>
+        <button class="btn btn-primary">Test Button</button>
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+</html>
