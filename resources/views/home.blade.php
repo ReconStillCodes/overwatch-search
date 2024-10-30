eeba7466a6cea432096e20c6c2c59f999b7c7852
Matthew <m.matthew.muliawan@gmail.com>
2024-10-30 12:01:24 +0700

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,18 +2,42 @@
 <html>
 <head>
     <title>Home</title>
-    <nav class="navbar navbar-expand-lg navbar-light"> 
-
-        <a class="navbar-brand mb-2 h1" href="#">OVERWATCH</a>
-
-        <button class="navbar-toggler" type="button" >
-    <span class="navbar-toggler-icon"></span>
-  </button>
-    </nav>
     <link href="/bootstrap-5.0.2-dist/css/bootstrap.css" rel="stylesheet">
 </head>
 <body>
-    <h1> test</h1>
-    <script src="/bootstrap-5.0.2-dist/js/bootstrap.js"></script>
+    <nav class="navbar">
+        <div class="container-xxl  fw-bold">
+            <a href="{{route('home')}}" class="navbar-nav">
+                <span class="text-primary"> 
+                    OVERWATCH
+                </span>
+            </a>
+        
+        </div>
+    </nav>
+    
+
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dd-roles" data-bs-toggle="dropdown">
+            ROLES
+        </button>
+        
+        <ul class="dropdown-menu" aria-labelledby="dd-roles">
+            <li><a href="#" class="dropdown-item">
+                    aseasdasd
+                </a>
+            </li>
+            <li><a href="#" class="dropdown-item">
+                    test
+                </a>
+            </li>
+            <li><a href="#" class="dropdown-item">
+                    test
+                </a>
+            </li>
+        </ul>
+    </div>
+    
+    <script src="/bootstrap-5.0.2-dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
